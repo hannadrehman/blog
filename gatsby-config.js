b440e9746a14bd21be54b735a05dec332865d698
hanadrehman <hanad.rehman@healthifyme.com>
2022-01-29 19:54:57 +0530

    adds new blog post

--- gatsby-config.js
+++ gatsby-config.js
@@ -178,5 +178,11 @@ module.exports = {
         openAnalyzer: false,
       },
     },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [`gatsby-remark-responsive-iframe`],
+     },
+    },
   ].filter(Boolean),
 };
